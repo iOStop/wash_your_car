d6638fa4d46dbaa145d25053d995dd3374f08f3c
rudneff <dmitryrudneff@gmail.com>
2016-10-30 20:13:14 +0300

    Added dependencies for analytics.

--- app/src/main/java/com/iostop/wash_your_car/MainActivity.java
+++ app/src/main/java/com/iostop/wash_your_car/MainActivity.java
@@ -3,7 +3,8 @@ package com.iostop.wash_your_car;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
